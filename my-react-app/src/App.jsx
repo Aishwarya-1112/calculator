42334370d9967848590ab2f86b53265dd466e1bc
Aishwarya1155 <aishwaryamanoharann@gmail.com>
2025-02-09 15:29:00 +0530

    final

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,125 +1,148 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  const [result, Setresult] = useState("");
+  const [result, setResult] = useState("");
+
   const click = (e) => {
-    Setresult(result.concat(e.target.name));
+    setResult(result.concat(e.target.name));
   };
   const clear = () => {
-    Setresult("");
+    setResult("");
   };
   const backspace = () => {
-    Setresult(result.slice(0, -1));
+    setResult(result.slice(0, -1));
   };
   const calculate = () => {
-    Setresult();
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("Error");
+    }
   };
-
   return (
     <>
       <div className="container">
-        <form action="">
+        <form>
           <input
             type="text"
             value={result}
           />
         </form>
-        <div className="keyboard">
-          <button onClick={clear}>Clear</button>
-          <button onClick={backspace}>C</button>
+
+        <div className="keypad">
           <button
-            name=""
+            className="highlight"
+            onClick={clear}
+            id="clear"
+          >
+            Clear
+          </button>
+          <button
+            className="highlight"
+            onClick={backspace}
+            id="backspace"
+          >
+            C
+          </button>
+          <button
+            className="highlight"
+            name="/"
             onClick={click}
           >
             &divide;
           </button>
           <button
-            name=""
+            name="7"
             onClick={click}
           >
             7
           </button>
           <button
-            name=""
+            name="8"
             onClick={click}
           >
             8
           </button>
           <button
-            name=""
+            name="9"
             onClick={click}
           >
             9
           </button>
           <button
-            name=""
+            className="highlight"
+            name="*"
             onClick={click}
           >
             &times;
           </button>
           <button
-            name=""
+            name="4"
             onClick={click}
           >
             4
           </button>
           <button
-            name=""
+            name="5"
             onClick={click}
           >
             5
           </button>
           <button
-            name=""
+            name="6"
             onClick={click}
           >
             6
           </button>
           <button
-            name=""
+            className="highlight"
+            name="-"
             onClick={click}
           >
             &ndash;
           </button>
           <button
-            name=""
+            name="1"
             onClick={click}
           >
             1
           </button>
           <button
-            name=""
+            name="2"
             onClick={click}
           >
             2
           </button>
           <button
-            name=""
+            name="3"
             onClick={click}
           >
             3
           </button>
           <button
-            name=""
+            className="highlight"
+            name="+"
             onClick={click}
           >
             +
           </button>
           <button
-            name=""
+            name="0"
             onClick={click}
           >
             0
           </button>
           <button
-            name=""
+            name="."
             onClick={click}
           >
             .
           </button>
           <button
-            name=""
-            onClick={click}
+            className="highlight"
+            onClick={calculate}
+            id="result"
           >
             =
           </button>
@@ -130,39 +153,3 @@ const App = () => {
 };
 
 export default App;
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
